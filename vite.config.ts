61525e6206edd500614f0a614807d1dc496d56d0
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2024-11-10 11:31:30 +0900

    chore: shadcn
    https://ui.shadcn.com/docs/installation/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,5 +9,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     base: env.VITE_BASE || "/todo-app/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   };
 });
