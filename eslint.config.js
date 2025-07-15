ea3d7fa1acc2af07d515ca361c44a0841c92c38c
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2025-07-15 20:21:15 +0900

    chore: migrate eslint storybook settings from package.json to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -27,4 +28,5 @@ export default tseslint.config(
       "react-refresh/only-export-components": "off",
     },
   },
+  ...storybook.configs["flat/recommended"],
 );
